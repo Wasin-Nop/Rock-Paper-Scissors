c32365483b9223277d87fd8bf5ae935ce5a6c780
wasin <62063835+wasin-nop@users.noreply.github.com>
2023-05-17 07:25:23 +0700

    Create playerSelection prompt and consolelog result

--- script.js
+++ script.js
@@ -4,36 +4,54 @@ function getComputerChoice() {
   console.log(`Computer choose ${randomRPS}`);
   return randomRPS;
 }
+const playerSelection = () =>
+  prompt("Enter your choice: rock, paper, or scissors", "");
 
-function play(playerSelection, computerSelection) {
-  if (!playerSelection) return "You're not choose anything";
+function playRound(playerSelection, computerSelection) {
+  if (!playerSelection) return "You're not choose anything or spell it wrong";
   const playerSelectionLowerCase = playerSelection.toLowerCase();
   const computerSelectionLowerCase = computerSelection.toLowerCase();
   if (playerSelectionLowerCase === "rock") {
     if (computerSelectionLowerCase === "paper") {
+      console.log("You Lose! Paper beats Rock");
       return "You Lose! Paper beats Rock";
     } else if (computerSelectionLowerCase === "scissors") {
+      console.log("You Win! Rock beats Scissors");
       return "You Win! Rock beats Scissors";
     } else {
+      console.log("Draw");
       return "Draw";
     }
   }
   if (playerSelectionLowerCase === "paper") {
     if (computerSelectionLowerCase === "scissors") {
+      console.log("You Lose! Scissors beats Paper");
       return "You Lose! Scissors beats Paper";
     } else if (computerSelectionLowerCase === "rock") {
+      console.log("You Win! Paper beats Rock");
       return "You Win! Paper beats Rock";
     } else {
+      console.log("Draw");
       return "Draw";
     }
   }
   if (playerSelectionLowerCase === "scissors") {
     if (computerSelectionLowerCase === "rock") {
+      console.log("You Lose! Rock beats Scissors");
       return "You Lose! Rock beats Scissors";
     } else if (computerSelectionLowerCase === "paper") {
+      console.log("You Win! Scissors beats Paper");
       return "You Win! Scissors beats Paper";
     } else {
+      console.log("Draw");
       return "Draw";
     }
+  } else {
+    console.log("You're not choose anything or spell it wrong");
   }
 }
+playRound(playerSelection(), getComputerChoice());
+playRound(playerSelection(), getComputerChoice());
+playRound(playerSelection(), getComputerChoice());
+playRound(playerSelection(), getComputerChoice());
+playRound(playerSelection(), getComputerChoice());
